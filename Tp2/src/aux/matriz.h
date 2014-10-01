fa58a60957d68adb2e5e413336d37d64c5484fea
Javier Nicolas Minces Muller <jminces@ws5.labo5>
2014-10-01 16:55:44 -0300

    hola

---
+++ Tp2/src/aux/matriz.h
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+typedef vector<double> fila;
+
+struct Matriz
+{
+public:
+
+//generador basico, no se usa
+Matriz();
+
+//~Matriz();
+
+//generador que te crea una matriz de un tamaño, no pide vectores
+
+Matriz( int CFilas, int Ccolumnas);
+
+//generador que te crea la matriz con los vectores que le pasaste
+Matriz( vector< vector<double> > filas, int cantfilas, int cantcolumnas, vector<double> indeps, vector<int> pos);
+
+void Definir(double adefinir, int fila, int columna);
+
+double Posicion(int fila, int columna);
+
+void mostrar();
+
+private:
+
+vector <double> _valores;
+
+vector <int> _posValores;
+
+vector <int> _inicioFilas;
+
+int _Cfilas;
+
+int _Ccolumnas;
+
+};
