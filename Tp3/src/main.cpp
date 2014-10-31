bf397dd17e9598f77663006f7f4e58ecafdacf14
Karamchi <karamchi.ghandi@gmail.com>
2014-10-31 02:00:24 -0300

    Aprendiendo a leer y escribir

--- Tp3/src/main.cpp
+++ Tp3/src/main.cpp
@@ -1,17 +1,85 @@
 #include <iostream>
-#include <ctime>
+#include <fstream>
 #include "aux/ops.cpp"
+#include <ctime>
 
 using namespace std;
 
-int main(){
+void cargar(Matriz& m, vector<unsigned char>& a, char* in) {
+	ifstream image;
+	image.open(in, ios_base::binary);
+
+	image.seekg (0, ios::end);
+	int n = image.tellg();
+	image.seekg (0, ios::beg);
+
+	char* res = new char[n];
+	for(int i = 0; i < n; i++)
+	res[i] = '0';
+
+	image.read(res, n); //hasta acá, gracias gugl
+
+	for (int i=0;i<54;i++) {
+		a[i]=res[i];
+	} //el header hay que guardarlo para generar una nueva imagen
+
+	for (int i=54;i<n;i+=3) {
+		pixel p;
+		char x=res[i];
+		int ix=(int) x;
+		p.blue=(ix+256)*(ix<0)+ix;
+		x=res[i+1];
+		ix=(int) x;
+		p.green=(ix+256)*(ix<0)+ix;
+		x=res[i+2];
+		ix=(int) x;
+		p.red=(ix+256)*(ix<0)+ix; //¿Little endian o lo que muestra me corta el borde?
+        int ireal=(i-54)/3;
+        m.Definir(p,(ireal/512)+1,(ireal%512)+1);
+	} 
+}
+
+void devolver(Matriz& m, vector<unsigned char>& a, char* out) {
+    int w=768;
+    int h=512;
+    FILE *f;
+    unsigned char img[3*w*h+54];
+
+    for (int i=0;i<54;i++) {
+        img[i]=a[i];
+    }
+
+    for (int i=0;i<w;i++) {
+        for (int j=0;j<h;j++) {
+            int r = m.Posicion(i+1,j+1).red;
+            int g = m.Posicion(i+1,j+1).green;
+            int b = m.Posicion(i+1,j+1).blue;
+            img[54+(i+j*w)*3+2] = (unsigned char)(r); // estará bien que esté al vesre? ver http://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries
+            img[54+(i+j*w)*3+1] = (unsigned char)(g);
+            img[54+(i+j*w)*3+0] = (unsigned char)(b);
+        }
+    }
+
+    f = fopen(out,"wb");
+    fwrite(img,1,54+w*h*3,f);
+    fclose(f);
+}
+
+int main(int argc, char** argv) {
 	/*clock_t t;
-	t = clock();
-	ofstream tiempo;
+	t = clock();*/
+
+    Matriz m(768,512);
+    vector<unsigned char> header(54);
+	cargar(m,header,argv[1]);
+    devolver(m,header,argv[2]); //podemos usar argv[3] para el método
+
+	/*ofstream tiempo;
 	t = clock() - t;
 	tiempo.open("Tiempo");
-	tiempo << "Clocks: "<< (long int)t << " segundos: " << ((float)t)/CLOCKS_PER_SEC << endl;
-	pixel p;
+	tiempo << "Clocks: "<< (long int)t << " segundos: " << ((float)t)/CLOCKS_PER_SEC << endl;*/
+
+    /*pixel p;
 	Matriz m(20,10);
 	for (int i=0;i<20;i++) {
 		for (int j=0;j<10;j++) {
@@ -29,7 +97,3 @@ int main(){
 
 	return 0;
 }
-
-
-
-
