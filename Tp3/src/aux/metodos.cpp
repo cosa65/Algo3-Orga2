0dea4aa943a4ff52aa978d6987f2669bedd29a7b
cosa65 <mfosc65@gmail.com>
2014-11-03 15:09:00 -0300

    ahora trabaja sobre una nueva matriz :D

--- Tp3/src/aux/metodos.cpp
+++ Tp3/src/aux/metodos.cpp
@@ -4,12 +4,22 @@
 //Los hago void y por referencia porque no necesitamos guardar la matriz mosaiqueada para el psnr.
 
 void VecinoMasCercano(Matriz& mat){
+
+	Matriz nuevamat(mat.Cfilas(), mat.Ccolumnas());
+
+	for (int i=1; i<=mat.Cfilas(); i++){
+		for (int j=1; j<=mat.Ccolumnas(); j++){
+			nuevamat.Definir(mat.Posicion(i,j),i,j);
+		}
+	}
+
+
 	for (int i=1;i<=mat.Cfilas()/2;i++) {
 		for (int j=1;j<=mat.Ccolumnas()/2;j++) {
 		    pixel pr=mat.Posicion(i*2-1,j*2-1); // rojo
 		    pixel pg=mat.Posicion(i*2-1,j*2); //verde
 		    pr.green=pg.green;
-		    mat.Definir(pr,i*2-1,j*2-1);
+		    nuevamat.Definir(pr,i*2-1,j*2-1);
 //          mat.Definir(pg,i*2-1,j*2);
 		}
 		for (int j=1;j<=mat.Ccolumnas()/2;j++) {
@@ -17,24 +27,36 @@ void VecinoMasCercano(Matriz& mat){
 		    pixel pb=mat.Posicion(i*2,j*2); //azul
 		    pb.green=pg.green;
 //          mat.Definir(pg,i*2,j*2-1);
-		    mat.Definir(pb,i*2,j*2);
+		    nuevamat.Definir(pb,i*2,j*2);
 		}
 	}
+	mat = nuevamat;
 }
 
 void InterpBilineal(Matriz& mat){
+
+	Matriz nuevamat(mat.Cfilas(), mat.Ccolumnas());
+
+	for (int i=1; i<=mat.Cfilas(); i++){
+		for (int j=1; j<=mat.Ccolumnas(); j++){
+			nuevamat.Definir(mat.Posicion(i,j),i,j);
+		}
+	}
+
+
 	for (int i=1;i<=mat.Cfilas()/2;i++) {
 		for (int j=1;j<=mat.Ccolumnas()/2;j++) {
             pixel pr=mat.Posicion(i*2-1,j*2-1); // rojo
             pr.green=(mat.Posicion(i*2-1,j*2).green+mat.Posicion(i*2-1,j*2-2).green+mat.Posicion(i*2,j*2-1).green+mat.Posicion(i*2-2,j*2-1).green)/4;
-            mat.Definir(pr,i*2-1,j*2-1);
+            nuevamat.Definir(pr,i*2-1,j*2-1);
 		}
 		for (int j=1;j<=mat.Ccolumnas()/2;j++) {
             pixel pb=mat.Posicion(i*2,j*2); //azul
             pb.green=(mat.Posicion(i*2,j*2+1).green+mat.Posicion(i*2,j*2-1).green+mat.Posicion(i*2+1,j*2).green+mat.Posicion(i*2-1,j*2).green)/4;
-            mat.Definir(pb,i*2,j*2);
+            nuevamat.Definir(pb,i*2,j*2);
 		}
 	}
+	mat = nuevamat;
 }
 
 void InterpXDir(Matriz& mat){
@@ -46,8 +68,8 @@ void InterpXDir(Matriz& mat){
 	//	crear un vector p=[0,2,4,6...] (o modificar evaluarenInterpolante para que no sea necesario)
 	//	for (int j=1;j<n;j+=2) {
 	//		x[j]=evaluarenInterpolante(p,x,j);
-	//	y metés x en la matriz. También podés no trabajar sobre una copia del vector y poner Definir(eso,i,j);
-	//	pero de todas formas deberías trabajar sobre una copia de la matriz para poder definir el próximo paso
+	//	y metés x en la matriz. NO podés no trabajar sobre una copia del vector y poner Definir(eso,i,j);
+	//	TENES que trabajar sobre una copia de la matriz para poder definir el próximo paso
 	//	o bien hacer todo junto punto por punto, pero ahí tendrías que volver a calcular los vectores cada vez
 	// Si no hacés todo junto punto por punto:
 	//	Algo parecido para la vertical/diagonal/la que sea y la guardás en mat2
